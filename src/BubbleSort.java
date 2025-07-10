0aa2c0a359c0c12e5fa34af09d1d733e383a7359
Eros Coelho <eros.limacoelho@student.griffith.ie>
2025-07-10 14:08:07 +0100

    first commit

---
+++ src/BubbleSort.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+//        picking a random number between 10 and 20 and using it as the length for the array
+        int size = random.nextInt(11) + 10;
+        int[] array = new int[size];
+
+//        populating the array with random numbers between 1 and 100
+        for (int i = 0; i<size; i++){
+            array[i] = random.nextInt(100);
+        }
+
+//        printing the original array with toString
+        System.out.println("Original Array:");
+        System.out.println(Arrays.toString(array));
+
+        int comparisons = 0;
+        int swaps = 0;
+
+//        outer loop for bubblesort
+        for (int i = 0; i < array.length -1; i++){
+            boolean swapped = false;
+
+//            only checking the elements which haven't been checked yet
+            for (int j = 0; j < array.length - i - 1; j++){
+                comparisons++;
+
+//                actual sorting of elements in the array
+                if (array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
+                    swaps++;
+                    swapped = true;
+                }
+            }
+//            if there is nothing more to swap, stop the loop, since it is already sorted
+            if (!swapped){
+                break;
+            }
+        }
+        System.out.println("\nSorted Array:");
+        System.out.println(Arrays.toString(array));
+        System.out.println("\nComparisons: " + comparisons);
+        System.out.println("\nSwaps: " + swaps);
+    }
+}
