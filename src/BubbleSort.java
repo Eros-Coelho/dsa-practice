ac820e229f93b73fef5909fce163bd5f8bb23d18
Eros Coelho <eros.limacoelho@student.griffith.ie>
2025-09-14 11:26:38 +0100

    added a boolean helper method to avoid duplicates when generating the array elements on bubblesort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -3,6 +3,16 @@ import java.util.Random;
 
 public class BubbleSort {
 
+    //    helper method to check if the array already has an element before inserting into it
+    static boolean contains(int[] arr, int size, int value) {
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
 
         Random random = new Random();
@@ -14,9 +24,16 @@ public class BubbleSort {
 //        start measuring time
         long startTime = System.nanoTime();
 
-//        populating the array with random numbers between 1 and 100
-        for (int i = 0; i<size; i++){
-            array[i] = random.nextInt(100);
+//        populating the array with random numbers between 1 and 100 with no duplicates
+        for (int i = 0; i < size; i++) {
+            int element = random.nextInt(100);
+
+            if (!contains(array, i, element)) {
+                array[i] = element;
+            } else {
+                System.out.println("Duplicate elements: " + element);
+                i--;
+            }
         }
 
 //        printing the original array with toString
@@ -27,24 +44,24 @@ public class BubbleSort {
         int swaps = 0;
 
 //        outer loop for bubblesort
-        for (int i = 0; i < array.length -1; i++){
+        for (int i = 0; i < array.length - 1; i++) {
             boolean swapped = false;
 
 //            only checking the elements which haven't been checked yet
-            for (int j = 0; j < array.length - i - 1; j++){
+            for (int j = 0; j < array.length - i - 1; j++) {
                 comparisons++;
 
 //                actual sorting of elements in the array
-                if (array[j] > array[j+1]){
+                if (array[j] > array[j + 1]) {
                     int temp = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = temp;
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                     swaps++;
                     swapped = true;
                 }
             }
 //            if there is nothing more to swap, stop the loop, since it is already sorted
-            if (!swapped){
+            if (!swapped) {
                 break;
             }
         }
