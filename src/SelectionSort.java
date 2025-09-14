fd3e56d740e03dacead0f212dfe8c9d7b431b639
Eros Coelho <eros.limacoelho@student.griffith.ie>
2025-09-14 13:59:48 +0100

    also making so that the selection sort has no duplicate elements in the array

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,21 +1,40 @@
 import java.util.Arrays;
 import java.util.Random;
 
-public class SelectionSort{
+public class SelectionSort {
 
-//    Simple selection sort implementation first
+    //    helper method to check if the array already has an element before inserting into it
+    static boolean contains(int[] arr, int size, int value) {
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //    Simple selection sort implementation first
     public static void main(String[] args) {
 
         Random random = new Random();
 
-        int size = random.nextInt(11)+10;
+//        picking a random number between 10 and 20 to serve as array length
+        int size = random.nextInt(11) + 10;
         int[] array2 = new int[size];
 
-        //        start measuring time for the sorting
+//        start measuring time for the sorting
         long startTime = System.nanoTime();
 
-        for (int i = 0; i < array2.length; i++){
-            array2[i] = random.nextInt(100);
+        //        populating the array with random numbers between 1 and 100 with no duplicates
+        for (int i = 0; i < size; i++) {
+            int element = random.nextInt(100);
+
+            if (!contains(array2, i, element)) {
+                array2[i] = element;
+            } else {
+                System.out.println("Duplicate elements: " + element);
+                i--;
+            }
         }
 
         System.out.println("Original Array (Length: " + size + "):");
@@ -24,16 +43,16 @@ public class SelectionSort{
         int comparisons = 0;
         int swaps = 0;
 
-        for (int i = 0; i < array2.length - 1; i++){
+        for (int i = 0; i < array2.length - 1; i++) {
             int minIndex = i;
 
-            for (int j = i+1; j < array2.length; j++){
+            for (int j = i + 1; j < array2.length; j++) {
                 comparisons++;
-                if (array2[j] < array2[minIndex]){
+                if (array2[j] < array2[minIndex]) {
                     minIndex = j;
                 }
             }
-            if (minIndex != i){
+            if (minIndex != i) {
                 int temp = array2[i];
                 array2[i] = array2[minIndex];
                 array2[minIndex] = temp;
